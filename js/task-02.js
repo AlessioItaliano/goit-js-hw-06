cd24008175f1ecc9dd3bf47853b0e06a745576b8
Vasyl Lepish <117004345+AlessioItaliano@users.noreply.github.com>
2023-03-23 15:08:53 +0200

    task 2 ( third type)

--- js/task-02.js
+++ js/task-02.js
@@ -7,37 +7,67 @@ const ingredients = [
   "Condiments",
 ];
 
+//first type
+// const oldList = document.querySelector("#ingredients");
+
+// const firstElem = document.createElement("li");
+// firstElem.textContent = ingredients[0];
+// firstElem.setAttribute("class", "item");
+
+// const secondElem = document.createElement("li");
+// secondElem.textContent = ingredients[1];
+// secondElem.setAttribute("class", "item");
+
+// const thirdElem = document.createElement("li");
+// thirdElem.textContent = ingredients[2];
+// thirdElem.setAttribute("class", "item");
+
+// const fourthElem = document.createElement("li");
+// fourthElem.textContent = ingredients[3];
+// fourthElem.setAttribute("class", "item");
+
+// const fifthElem = document.createElement("li");
+// fifthElem.textContent = ingredients[4];
+// fifthElem.setAttribute("class", "item");
+
+// const sixthElem = document.createElement("li");
+// sixthElem.textContent = ingredients[5];
+// sixthElem.setAttribute("class", "item");
+
+// oldList.append(
+//   firstElem,
+//   secondElem,
+//   thirdElem,
+//   fourthElem,
+//   fifthElem,
+//   sixthElem
+// );
+
+//second type
+// const oldList = document.querySelector("#ingredients");
+
+// const newElements = ingredients.map((elem) => {
+//   const newItem = document.createElement("li");
+//   newItem.textContent = elem;
+//   newItem.setAttribute("class", "item");
+
+//   return newItem;
+// });
+
+// oldList.append(...newElements);
+
+//third type
 const oldList = document.querySelector("#ingredients");
 
-const firstElem = document.createElement("li");
-firstElem.textContent = ingredients[0];
-firstElem.setAttribute("class", "item");
+const createListOption = (elements) => {
+  return elements.map((elem) => {
+    const newItem = document.createElement("li");
+    newItem.textContent = elem;
+    newItem.setAttribute("class", "item");
 
-const secondElem = document.createElement("li");
-secondElem.textContent = ingredients[1];
-secondElem.setAttribute("class", "item");
+    return newItem;
+  });
+};
 
-const thirdElem = document.createElement("li");
-thirdElem.textContent = ingredients[2];
-thirdElem.setAttribute("class", "item");
-
-const fourthElem = document.createElement("li");
-fourthElem.textContent = ingredients[3];
-fourthElem.setAttribute("class", "item");
-
-const fifthElem = document.createElement("li");
-fifthElem.textContent = ingredients[4];
-fifthElem.setAttribute("class", "item");
-
-const sixthElem = document.createElement("li");
-sixthElem.textContent = ingredients[5];
-sixthElem.setAttribute("class", "item");
-
-oldList.append(
-  firstElem,
-  secondElem,
-  thirdElem,
-  fourthElem,
-  fifthElem,
-  sixthElem
-);
+const newListElements = createListOption(ingredients);
+oldList.append(...newListElements);
