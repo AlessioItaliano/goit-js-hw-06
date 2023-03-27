e13ec386c4477d1de030509bec54fc31846d464f
Vasyl Lepish <117004345+AlessioItaliano@users.noreply.github.com>
2023-03-27 18:08:31 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector(".login-form");
+console.log(loginForm);
 
+loginForm.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+
+  if (!(email.value && password.value)) {
+    alert("Всі поля повинні бути заповнені!");
+  } else {
+    const data = {
+      name: email.value,
+      password: password.value,
+    };
+    // console.log(data);
+  }
+}
