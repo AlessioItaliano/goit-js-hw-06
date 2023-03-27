88eb342a7153699307502d88aae7bfc8eb0be95f
Vasyl Lepish <117004345+AlessioItaliano@users.noreply.github.com>
2023-03-27 18:59:18 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,18 @@
+const widgetChangesBgCl = document.querySelector(".widget");
+const bodyChangesBgCl = document.querySelector("body");
+const spanNameChangesBgCl = document.querySelector(".color");
+console.log(spanNameChangesBgCl);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+widgetChangesBgCl.addEventListener("click", onBtnChangesBgCl);
+
+function onBtnChangesBgCl(event) {
+  event = getRandomHexColor();
+  bodyChangesBgCl.style.backgroundColor = event;
+  spanNameChangesBgCl.textContent = event;
 }
