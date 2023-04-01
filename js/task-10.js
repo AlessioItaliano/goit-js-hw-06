aeb994eccb2ccce1066be6b390d23242565c2fee
Vasyl Lepish <117004345+AlessioItaliano@users.noreply.github.com>
2023-04-01 11:00:42 +0300

    last

--- js/task-10.js
+++ js/task-10.js
@@ -6,20 +6,20 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function getSizeBoxValue() {}
 containerControls.children[1].addEventListener("click", createBoxes);
 
 function createBoxes() {
   const valueBoxes = Number(containerControls.children[0].value);
-  let sizeBox;
+  let boxInside;
 
   for (let i = 0; i <= valueBoxes; i += 1) {
-    sizeBox = 30 + i * 10;
+    let sizeBox = 30 + i * 10;
+    boxInside += `<div style="background-color:${getRandomHexColor()}" width="${sizeBox}px", heigth="${sizeBox}px"></div>`;
   }
 
-  boxContainer.insertAdjacentHTML(
-    "afterbegin",
-    `<div style="background-color:${getRandomHexColor()}" width="${sizeBox}px", heigth="${sizeBox}px"></div>`
-  );
+  boxContainer.insertAdjacentHTML("afterbegin", boxInside);
 }
 
 containerControls.children[2].addEventListener("click", destroyBoxes);
